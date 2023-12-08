2a330a48c7636ea1da9a9d2159e9f78c08e45845
unknown <nordeveloper@gmail.com>
2023-12-09 01:51:15 +0400

    -

--- config/app.php
+++ config/app.php
@@ -5,8 +5,6 @@ return [
     /*
     |--------------------------------------------------------------------------
     | Application Name
-    |--------------------------------------------------------------------------
-    |
     | This value is the name of your application. This value is used when the
     | framework needs to place the application's name in a notification or
     | any other location as required by the application or its packages.
@@ -20,8 +18,6 @@ return [
     /*
     |--------------------------------------------------------------------------
     | Application Environment
-    |--------------------------------------------------------------------------
-    |
     | This value determines the "environment" your application is currently
     | running in. This may determine how you prefer to configure various
     | services the application utilizes. Set this in your ".env" file.
