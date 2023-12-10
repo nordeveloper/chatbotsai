81d3b7ce9e47f1da149debee66bf242b2d382b4b
unknown <nordeveloper@gmail.com>
2023-12-10 17:40:17 +0400

    -

--- config/app.php
+++ config/app.php
@@ -29,8 +29,6 @@ return [
     /*
     |--------------------------------------------------------------------------
     | Application Debug Mode
-    |--------------------------------------------------------------------------
-    |
     | When your application is in debug mode, detailed error messages with
     | stack traces will be shown on every error that occurs within your
     | application. If disabled, a simple generic error page is shown.
@@ -42,8 +40,6 @@ return [
     /*
     |--------------------------------------------------------------------------
     | Application URL
-    |--------------------------------------------------------------------------
-    |
     | This URL is used by the console to properly generate URLs when using
     | the Artisan command line tool. You should set this to the root of
     | your application so that it is used when running Artisan tasks.
@@ -57,8 +53,6 @@ return [
     /*
     |--------------------------------------------------------------------------
     | Application Timezone
-    |--------------------------------------------------------------------------
-    |
     | Here you may specify the default timezone for your application, which
     | will be used by the PHP date and date-time functions. We have gone
     | ahead and set this to a sensible default for you out of the box.
